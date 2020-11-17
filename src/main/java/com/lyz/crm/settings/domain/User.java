3c24f760624148131a6223b5c15bdcb6a5e0b6d9
lyz <lyzgiser@163.com>
2020-11-17 15:48:44 +0800

    搭建项目基本环境，创建包结构

---
+++ src/main/java/com/lyz/crm/settings/domain/User.java
@@ -0,0 +1,159 @@
+package com.lyz.crm.settings.domain;
+
+public class User {
+    //id,数据库主键
+    private String id;
+
+    //登录账户
+    private String loginAct;
+
+    //用户名
+    private String name;
+
+    //密码
+    private String loginPwd;
+
+    //邮箱
+    private String email;
+
+    //失效时间
+    private String expireTime;
+
+    //锁定状态
+    private String lockState;
+
+    private String deptno;
+
+    private String allowIps;
+
+    private String createTime;
+
+    private String createBy;
+
+    private String editTime;
+
+    private String editBy;
+
+    public User() {
+    }
+
+    public User(String id, String loginAct, String name, String loginPwd, String email, String expireTime, String lockState, String deptno, String allowIps, String createTime, String createBy, String editTime, String editBy) {
+        this.id = id;
+        this.loginAct = loginAct;
+        this.name = name;
+        this.loginPwd = loginPwd;
+        this.email = email;
+        this.expireTime = expireTime;
+        this.lockState = lockState;
+        this.deptno = deptno;
+        this.allowIps = allowIps;
+        this.createTime = createTime;
+        this.createBy = createBy;
+        this.editTime = editTime;
+        this.editBy = editBy;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLoginAct() {
+        return loginAct;
+    }
+
+    public void setLoginAct(String loginAct) {
+        this.loginAct = loginAct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLoginPwd() {
+        return loginPwd;
+    }
+
+    public void setLoginPwd(String loginPwd) {
+        this.loginPwd = loginPwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getExpireTime() {
+        return expireTime;
+    }
+
+    public void setExpireTime(String expireTime) {
+        this.expireTime = expireTime;
+    }
+
+    public String getLockState() {
+        return lockState;
+    }
+
+    public void setLockState(String lockState) {
+        this.lockState = lockState;
+    }
+
+    public String getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(String deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getAllowIps() {
+        return allowIps;
+    }
+
+    public void setAllowIps(String allowIps) {
+        this.allowIps = allowIps;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public String getEditTime() {
+        return editTime;
+    }
+
+    public void setEditTime(String editTime) {
+        this.editTime = editTime;
+    }
+
+    public String getEditBy() {
+        return editBy;
+    }
+
+    public void setEditBy(String editBy) {
+        this.editBy = editBy;
+    }
+}
